e6e6f3b9d96e1dc111a265101e8ba3a435ba8b73
Luis <m.luishernandez@outlook.com>
2021-02-15 14:13:21 -0500

    added range

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,39 +1,28 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
 
-//Get Route
-//Aggregate function for get routes
 
-// View the total duration of each workout from the past seven workouts on the stats page.
-
-// Add exercises to the most recent workout plan.
 router.post("/api/workouts/", ({ body }, res) => {
     //This will create a workout based on the scheema
     Workout.create(body)
-
-        .then(result => {
-            console.log(result);
-            res.json(result);
-        })
-
-        .catch(err => {
-            res.json(err);
-        });
+    
+    .then(result => {
+        res.json(result);
+    })
+    .catch(err => {
+        res.json(err);
+    });
 });
 
-// Add new exercises to a new workout plan.
 router.put("/api/workouts/:id", (req, res) => {
-    Workout.findOneAndUpdate(
+    Workout.findByIdAndUpdate(
         { _id: req.params.id },
         
         {
             $push: {
                 exercises: req.body
             },
-            
-            $inc: {
-                totalduration : req.body.duration
-            }
+           
         }
         )
         .then(result => {
@@ -47,20 +36,37 @@ router.put("/api/workouts/:id", (req, res) => {
         
     });
     
-    // View the combined weight of multiple exercises from the past seven workouts on the stats page.
     
-    router.get("/api/workouts/", (req, res) => {
-        Workout.find({})
+    // router.get("/api/workouts/", (req, res) => {
+    //     Workout.find({})
+        
+    //     .then(result => {
+    //         console.log(result);
+    //         res.json(result);
+    //     })
+        
+    //     .catch(err => {
+    //         res.json(err);
+    //     });
+        
+    // });
     
-            .then(result => {
-                console.log(result);
-                res.json(result);
-            })
+    // router.get("/api/workouts/range", (req, res) => {
+
+    //     Workout.find({})
+    //     .limit(7)
     
-            .catch(err => {
-                res.json(err);
-            });
-    
-    });
-    
-module.exports = router;
+    //     .then(result => {
+    //         console.log(result);
+    //         res.json(result);
+    //     })
+        
+    //     .catch(err => {
+    //         res.json(err);
+    //     })
+
+    // });
+
+
+
+    module.exports = router;
